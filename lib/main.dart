3177e32c3b8ed7acb62c77275020edd5f583c3ae
TodorMarkov88 <todor.markoww@gmail.com>
2023-01-26 14:00:45 +0200

    flutter-shop-v1.2

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
+    return MultiProvider( 
       providers: [
         ChangeNotifierProvider(
           create: (_) => Auth(),
